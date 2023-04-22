d0a12a5d3d7ce164500efa701f25e089de44bf6a
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2023-04-21 20:11:20 -0700

    added swtich between dbstorage and filestorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of Dbstorage if it exists
+or filestorage if it doesnt exist"""
+
 from models.engine.file_storage import FileStorage
+from os import getenv
+from models.engine.db_storage import DBStorage
 
-
-storage = FileStorage()
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
