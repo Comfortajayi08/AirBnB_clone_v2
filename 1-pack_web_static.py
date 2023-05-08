234163029094bf1b427e8c046e03daaa918f7b65
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2023-05-07 19:10:29 -0700

    using fabric to generate a tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tg archive from the contents of the
+    web statics folder. ChatGPT generated code"""
+
+from fabric.api import local, env, run
+from datetime import datetime
+
+env.hosts = ['localhost']
+env.user = 'your_username'
+
+def do_pack():
+    """Generate a .tgz archive from the contents of the web_static folder."""
+
+    now = datetime.now()
+    timestamp = now.strftime('%Y%m%d%H%M%S')
+    archive_path = 'versions/web_static_{}.tgz'.format(timestamp)
+
+    local('mkdir -p versions')
+
+    result = local('tar -czvf {} web_static'.format(archive_path))
+    if result.failed:
+        return None
+    return archive_path
+
